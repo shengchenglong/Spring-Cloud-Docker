431887e135dbb46d640bd845fb0c01f13119ac4d
盛承龙 <shengchenglong@fulan.com.cn>
2017-08-16 15:08:29 +0800

    修改eureka.client.register-with-eureka=false

--- microservice-discovery-eureka-ha/src/main/java/com/scl/cloud/MicroserviceDiscoveryEurekaHaApplication.java
+++ microservice-discovery-eureka-ha/src/main/java/com/scl/cloud/MicroserviceDiscoveryEurekaHaApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class MicroserviceDiscoveryEurekaApplication {
+public class MicroserviceDiscoveryEurekaHaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceDiscoveryEurekaApplication.class, args);
+		SpringApplication.run(MicroserviceDiscoveryEurekaHaApplication.class, args);
 	}
 }
--- microservice-discovery-eureka-hb/src/main/java/com/scl/cloud/MicroserviceDiscoveryEurekaHbApplication.java
+++ microservice-discovery-eureka-hb/src/main/java/com/scl/cloud/MicroserviceDiscoveryEurekaHbApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class MicroserviceDiscoveryEurekaApplication {
+public class MicroserviceDiscoveryEurekaHbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceDiscoveryEurekaApplication.class, args);
+		SpringApplication.run(MicroserviceDiscoveryEurekaHbApplication.class, args);
 	}
 }
