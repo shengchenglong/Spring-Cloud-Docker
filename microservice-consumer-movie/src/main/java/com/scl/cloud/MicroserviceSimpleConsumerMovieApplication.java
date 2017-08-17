d462148f0293a31911cac1f10b3f46caeb6c903c
盛承龙 <shengchenglong@fulan.com.cn>
2017-08-17 13:51:36 +0800

    整合Ribbon

--- microservice-consumer-movie/src/main/java/com/scl/cloud/MicroserviceSimpleConsumerMovieApplication.java
+++ microservice-consumer-movie/src/main/java/com/scl/cloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -3,6 +3,7 @@ package com.scl.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +16,7 @@ public class MicroserviceSimpleConsumerMovieApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- microservice-consumer-movie/src/main/java/com/scl/cloud/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/scl/cloud/controller/MovieController.java
@@ -1,11 +1,18 @@
 package com.scl.cloud.controller;
 
 import com.scl.cloud.pojo.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author shengchenglong
@@ -15,11 +22,30 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MovieController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
+
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @GetMapping("/user/{id}")
     public User findByUserId(@PathVariable Long id) {
-        return this.restTemplate.getForObject("http://localhost:10000/" + id, User.class);
+        return this.restTemplate.getForObject("http://microservice-provider-user/" + id, User.class);
+    }
+
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo() {
+        return this.discoveryClient.getInstances("microservice-provider-user");
+    }
+
+    @GetMapping("/log-instance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+        LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
     }
 }
