0c853eb21cd6c536d1e38a2757760879418936b8
盛承龙 <shengchenglong@fulan.com.cn>
2017-08-29 17:47:43 +0800

    Feign

--- microservice-consumer-movie/src/main/java/com/scl/cloud/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/scl/cloud/controller/MovieController.java
@@ -1,18 +1,17 @@
 package com.scl.cloud.controller;
 
 import com.scl.cloud.pojo.User;
+import com.scl.cloud.feign.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+//import org.springframework.cloud.client.discovery.DiscoveryClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
 
 /**
  * @author shengchenglong
@@ -24,28 +23,36 @@ public class MovieController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
 
-    @Autowired
-    private RestTemplate restTemplate;
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+//
+//    @Autowired
+//    private LoadBalancerClient loadBalancerClient;
+//
+//    @GetMapping("/user/{id}")
+//    public User findByUserId(@PathVariable Long id) {
+//        return this.restTemplate.getForObject("http://microservice-provider-user/" + id, User.class);
+//    }
+//
+//    @GetMapping("/user-instance")
+//    public List<ServiceInstance> showInfo() {
+//        return this.discoveryClient.getInstances("microservice-provider-user");
+//    }
+//
+//    @GetMapping("/log-instance")
+//    public void logUserInstance() {
+//        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+//        LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+//    }
 
     @Autowired
-    private DiscoveryClient discoveryClient;
+    private UserFeignClient userFeignClient;
 
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;
-
-    @GetMapping("/user/{id}")
-    public User findByUserId(@PathVariable Long id) {
-        return this.restTemplate.getForObject("http://microservice-provider-user/" + id, User.class);
-    }
-
-    @GetMapping("/user-instance")
-    public List<ServiceInstance> showInfo() {
-        return this.discoveryClient.getInstances("microservice-provider-user");
-    }
-
-    @GetMapping("/log-instance")
-    public void logUserInstance() {
-        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
-        LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+    @GetMapping("user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.userFeignClient.findById(id);
     }
 }
--- microservice-consumer-movie/src/main/java/com/scl/cloud/feign/UserFeignClient.java
+++ microservice-consumer-movie/src/main/java/com/scl/cloud/feign/UserFeignClient.java
@@ -1,5 +1,7 @@
-package com.scl.cloud.rest;
+package com.scl.cloud.feign;
 
+import feign.Param;
+import feign.RequestLine;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,12 +18,18 @@ import com.scl.cloud.pojo.User;
 
 // @FeignClient注解的name是一个任意的客户端名称，用于创建Ribbon负载均衡器
 // 在本例中，使用了Eureka，则Ribbon会把microservice-provider-user解析成Eureka Server服务注册表中的服务
-@FeignClient(name = "microservice-provider-user")
+//@FeignClient(name = "microservice-provider-user")
+// 自定义Feign配置
+@FeignClient(name = "microservice-provider-user", configuration = FeignConfiguration.class)
 // 还可以使用url属性指定请求URL
 //@FeignClient(name = "microservice-provider-user", url = "http://localhost:9999/")
 public interface UserFeignClient {
-	
-	@GetMapping(value = "/{id}")
-	public User findById(@PathVariable("id") Long id);
 
+//	SpringMvc的注解
+//	@GetMapping(value = "{id}")
+//	public User findById(@PathVariable("id") Long id);
+
+//	使用Feign自带的注解
+	@RequestLine("GET /{id}")
+	public User findById(@Param("id") Long id);
 }
